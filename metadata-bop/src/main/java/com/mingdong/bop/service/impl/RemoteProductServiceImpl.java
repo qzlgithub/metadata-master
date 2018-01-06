05ea45eea56f9942429850285d0c0db1b81bd387
Jun-Z <junz0579@hotmail.com>
2018-01-06 18:30:42 +0800

    project created

--- metadata-bop/src/main/java/com/mingdong/bop/service/impl/RemoteProductServiceImpl.java
+++ metadata-bop/src/main/java/com/mingdong/bop/service/impl/RemoteProductServiceImpl.java
@@ -1,12 +1,17 @@
 package com.mingdong.bop.service.impl;
 
 import com.mingdong.core.service.RemoteProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RemoteProductServiceImpl implements RemoteProductService
 {
+    private static Logger logger = LoggerFactory.getLogger(RemoteProductServiceImpl.class);
+
     @Override
     public String sayHi(String name)
     {
+        logger.info("####### coming: {}", name);
         return "Hello " + name + "!";
     }
 }
--- metadata-csp/src/main/java/com/mingdong/csp/controller/IndexController.java
+++ metadata-csp/src/main/java/com/mingdong/csp/controller/IndexController.java
@@ -1,9 +1,27 @@
 package com.mingdong.csp.controller;
 
+import com.mingdong.core.service.RemoteProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
 
 @Controller
 public class IndexController
 {
+    private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+    @Resource
+    private RemoteProductService productApi;
+
+    @GetMapping(value = "/test")
+    public void test(@RequestParam(value = "name") String name)
+    {
+        logger.info(">>>>> call: {}", name);
+        String back = productApi.sayHi(name);
+        logger.info(">>>>> back: {}", back);
+    }
 
 }
