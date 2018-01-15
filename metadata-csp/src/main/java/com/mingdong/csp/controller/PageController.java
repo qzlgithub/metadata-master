c0f91dd0707f67a550af82c1d7556def072bbaa9
yuzy <451933467@qq.com>
2018-01-15 11:10:33 +0800

    page update

--- metadata-csp/src/main/java/com/mingdong/csp/controller/PageController.java
+++ metadata-csp/src/main/java/com/mingdong/csp/controller/PageController.java
@@ -1,8 +1,6 @@
 package com.mingdong.csp.controller;
 
 import com.mingdong.core.annotation.LoginRequired;
-import com.mingdong.core.model.BLResp;
-import com.mingdong.csp.model.RequestThread;
 import com.mingdong.csp.service.ClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +14,14 @@ public class PageController
     @Resource
     private ClientService clientService;
 
-    @GetMapping(value = {"/", "index.html"})
+    /**
+     * 登陆页
+     */
+    @GetMapping(value = {"/", "/index.html"})
     public ModelAndView indexPage()
     {
-        return new ModelAndView("index");
+        ModelAndView modelAndView = new ModelAndView("/index");
+        return modelAndView;
     }
 
     /**
@@ -27,11 +29,87 @@ public class PageController
      */
     @LoginRequired
     @GetMapping(value = {"/home.html"})
-    public BLResp getHomeData()
+    public ModelAndView getHomeData()
     {
-        BLResp resp = BLResp.build();
-        clientService.getHomeData(RequestThread.getClientId(), RequestThread.getUserId(), resp);
-        return resp;
+//        BLResp resp = BLResp.build();
+//        clientService.getHomeData(RequestThread.getClientId(), RequestThread.getUserId(), resp);
+
+        ModelAndView modelAndView = new ModelAndView("/home");
+
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/all.html"})
+    public ModelAndView productAll()
+    {
+
+        ModelAndView modelAndView = new ModelAndView("/product/all");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/detail.html"})
+    public ModelAndView productDetail()
+    {
+        ModelAndView modelAndView = new ModelAndView("/product/detail");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/introduce.html"})
+    public ModelAndView productIntroduce()
+    {
+        ModelAndView modelAndView = new ModelAndView("/product/introduce");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/mine.html"})
+    public ModelAndView productMine()
+    {
+        ModelAndView modelAndView = new ModelAndView("/product/mine");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/recharge.html"})
+    public ModelAndView productRecharge()
+    {
+        ModelAndView modelAndView = new ModelAndView("/product/recharge");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/product/request.html"})
+    public ModelAndView productRequest()
+    {
+        ModelAndView modelAndView = new ModelAndView("/product/request");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/system/account-list.html"})
+    public ModelAndView systemAccountList()
+    {
+        ModelAndView modelAndView = new ModelAndView("/system/account-list");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/system/account-set.html"})
+    public ModelAndView systemAccountSet()
+    {
+        ModelAndView modelAndView = new ModelAndView("/system/account-set");
+        return modelAndView;
+    }
+
+    @LoginRequired
+    @GetMapping(value = {"/system/message.html"})
+    public ModelAndView systemMessage()
+    {
+        ModelAndView modelAndView = new ModelAndView("/system/message");
+        return modelAndView;
     }
 
 }
