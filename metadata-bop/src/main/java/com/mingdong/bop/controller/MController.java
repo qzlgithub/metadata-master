5814286754c736cbe437ab3ecf8622093b513a77
Jun-Z <junz0579@hotmail.com>
2018-03-28 19:24:50 +0800

    code review

--- metadata-bop/src/main/java/com/mingdong/bop/controller/MController.java
+++ metadata-bop/src/main/java/com/mingdong/bop/controller/MController.java
@@ -1,5 +1,6 @@
 package com.mingdong.bop.controller;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.mingdong.bop.component.RedisDao;
 import com.mingdong.bop.constant.Field;
 import com.mingdong.bop.model.ManagerSession;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import sun.misc.BASE64Encoder;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +34,8 @@ public class MController
     private RedisDao redisDao;
     @Resource
     private SystemService systemService;
+    @Resource
+    private DefaultKaptcha kaptchaBuilder;
 
     @GetMapping(value = {"/", "/index.html", "/login.html"})
     public ModelAndView index(HttpServletRequest request) throws IOException
@@ -57,12 +64,23 @@ public class MController
     @ResponseBody
     public Map<String, Object> getImageCode(HttpServletRequest request) throws IOException
     {
-        ImageCode imageCode = CaptchaUtils.buildImageCode();
+        Map<String, Object> map = new HashMap<>();
+        String txt = StringUtils.getRandomString(4);
+        HttpSession session = request.getSession();
+        session.setAttribute(Field.IMAGE_CAPTCHA, txt);
+        BufferedImage image = kaptchaBuilder.createImage(txt);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", outputStream);
+        BASE64Encoder encoder = new BASE64Encoder();
+        String pic64 = encoder.encode(outputStream.toByteArray());
+        map.put(Field.IMAGE, "data:image/png;base64," + pic64);
+
+        /*ImageCode imageCode = CaptchaUtils.buildImageCode();
         Map<String, Object> map = new HashMap<>();
         map.put(Field.IMAGE, "data:image/png;base64," + imageCode.getBase64Code());
         HttpSession session = request.getSession();
         session.setAttribute(Field.IMAGE_CAPTCHA, imageCode.getCode());
-        System.out.println(">>>>>>>>: " + imageCode.getCode());
+        System.out.println(">>>>>>>>: " + imageCode.getCode());*/
         return map;
     }
 
