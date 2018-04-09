78031e6f55c1fde44810c58e6e2bd15ade06e97d
yuzy <451933467@qq.com>
2018-04-09 11:03:36 +0800

    update

--- metadata-bop/src/main/java/com/mingdong/bop/scheduled/CleanTrafficJob.java
+++ metadata-bop/src/main/java/com/mingdong/bop/scheduled/CleanTrafficJob.java
@@ -4,14 +4,13 @@ import com.mingdong.bop.configurer.ApplicationContextProvider;
 import com.mingdong.bop.service.QuartzService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 import java.util.Date;
 
 public class CleanTrafficJob implements Job
 {
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException
+    public void execute(JobExecutionContext jobExecutionContext)
     {
         try
         {
