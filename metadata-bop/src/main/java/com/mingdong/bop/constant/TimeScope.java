4bc57aa474f29e41673ba7605db00864813b88e1
Jun-Z <junz0579@hotmail.com>
2018-04-18 09:40:22 +0800

    code review

---
+++ metadata-bop/src/main/java/com/mingdong/bop/constant/TimeScope.java
@@ -0,0 +1,36 @@
+package com.mingdong.bop.constant;
+
+public enum TimeScope
+{
+    CUSTOM(0),
+    TODAY(1),
+    YESTERDAY(2),
+    LATEST_7_DAYS(3),
+    LATEST_30_DAYS(4),
+    LATEST_90_DAYS(5),
+    LATEST_1_YEAR(6);
+
+    private final int code;
+
+    TimeScope(int code)
+    {
+        this.code = code;
+    }
+
+    public static TimeScope getByCode(int code)
+    {
+        for(TimeScope o : TimeScope.values())
+        {
+            if(code == o.code)
+            {
+                return o;
+            }
+        }
+        return CUSTOM;
+    }
+
+    public int getCode()
+    {
+        return code;
+    }
+}
