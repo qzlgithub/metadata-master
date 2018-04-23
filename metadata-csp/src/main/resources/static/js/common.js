a7d314e2af2ce1d156a8c44c460df553b50a4ab5
Jun-Z <junz0579@hotmail.com>
2018-04-23 19:20:42 +0800

    用户修改密码成功后强制退出

--- metadata-csp/src/main/resources/static/js/common.js
+++ metadata-csp/src/main/resources/static/js/common.js
@@ -2,24 +2,34 @@ $(function() {
     $("#greeting").text(getGreeting());
     settingQq();
     var haveMessage = sessionStorage.getItem("have_message");
-    if(haveMessage === "1"){
+    if(haveMessage === "1") {
         $("#message-right-id").append('<em class="email-icon"></em>');
     }
 });
-function settingQq(){
+
+function settingQq() {
     var managerQq = sessionStorage.getItem("manager_qq");
     var serviceQq = sessionStorage.getItem("service_qq");
-    $(".setting-qq-class").attr("href","http://wpa.qq.com/msgrd?v=3&uin="+managerQq+"&site=qq&menu=yes");
-    $(".service-qq-class").attr("href","http://wpa.qq.com/msgrd?v=3&uin="+serviceQq+"&site=qq&menu=yes");
+    $(".setting-qq-class").attr("href", "http://wpa.qq.com/msgrd?v=3&uin=" + managerQq + "&site=qq&menu=yes");
+    $(".service-qq-class").attr("href", "http://wpa.qq.com/msgrd?v=3&uin=" + serviceQq + "&site=qq&menu=yes");
 }
+
 $("#exit").click(function() {
-    $.ajax({
-        type: "post",
-        url: "/client/user/logout",
-        dataType: "json",
-        contentType: "application/json",
-        success: function() {
-            window.location.href = "/login.html";
+    layer.confirm('确定退出系统？', {
+        btn: ['确定', '取消'],
+        yes: function() {
+            $.ajax({
+                type: "post",
+                url: "/client/user/logout",
+                dataType: "json",
+                contentType: "application/json",
+                success: function() {
+                    window.location.href = "/";
+                }
+            });
+        },
+        no: function() {
+            layer.closeAll();
         }
     });
 });
